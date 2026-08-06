33d336e6ccd411e4c0be4b4cd7a90c0455475488
lollolhappy826-lgtm <lollolhappy826@gmail.com>
2026-08-06 12:01:57 +0900

    money.js 만들기

---
+++ money.js
@@ -0,0 +1,35 @@
+let balance = 1248530;
+
+const balanceText = document.getElementById("balance");
+
+function updateBalance() {
+    balanceText.textContent = "₱" + balance.toLocaleString();
+}
+
+function earn(min, max, job) {
+    const money = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    balance += money;
+
+    updateBalance();
+
+    alert(`${job}\n\n+₱${money.toLocaleString()} 획득!`);
+}
+
+document.getElementById("parttime").onclick = () => {
+    earn(300, 700, "💼 알바하기");
+};
+
+document.getElementById("company").onclick = () => {
+    earn(800, 2000, "🏢 회사 다니기");
+};
+
+document.getElementById("royal").onclick = () => {
+    earn(3000, 8000, "👑 황궁 아르바이트");
+};
+
+document.getElementById("beg").onclick = () => {
+    earn(0, 100, "🎁 구걸하기");
+};
+
+updateBalance();
